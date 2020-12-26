42e1e5c4defaea0cde2024d8bb2c8f1130f396ac
Théo Roton <theo.roton@atilf.fr>
2020-12-26 18:14:42 +0100

    Commentaires Activite et Lieu

--- src/bean/Activite.java
+++ src/bean/Activite.java
@@ -26,27 +26,66 @@ public class Activite {
 	 */
 	private String nomLieu;
 	
+	/**
+	 * Méthode getter de l'id
+	 * @return id de l'activité
+	 */
 	public int getId() {
 		return id;
 	}
+	
+	/**
+	 * Méthode setter de l'id
+	 * @param id de l'activité
+	 */
 	public void setId(int id) {
 		this.id = id;
 	}
+	
+	/**
+	 * Méthode getter de la date de début
+	 * @return dateDebut de l'activité
+	 */
 	public Timestamp getDateDebut() {
 		return dateDebut;
 	}
+	
+	/**
+	 * Méthode setter de la date de début
+	 * @param dateDebut de l'activité
+	 */
 	public void setDateDebut(Timestamp dateDebut) {
 		this.dateDebut = dateDebut;
 	}
+	
+	/**
+	 * Méthode getter de la date de fin
+	 * @return dateFin de l'activité
+	 */
 	public Timestamp getDateFin() {
 		return dateFin;
 	}
+	
+	/**
+	 * Méthode setter de la date de fin
+	 * @param dateFin de l'activité
+	 */
 	public void setDateFin(Timestamp dateFin) {
 		this.dateFin = dateFin;
 	}
+	
+	/**
+	 * Méthode getter du nom du lieu
+	 * @return nomLieu de l'activité
+	 */
 	public String getNomLieu() {
 		return nomLieu;
 	}
+	
+	/**
+	 * Méthode setter du nom du lieu
+	 * @param nomLieu de l'activité
+	 */
 	public void setNomLieu(String nomLieu) {
 		this.nomLieu = nomLieu;
 	}
--- src/bean/Lieu.java
+++ src/bean/Lieu.java
@@ -9,27 +9,66 @@ public class Lieu {
 	private String adresse;
 	private List<Activite> activites;
 	
+	/**
+	 * Méthode getter de l'id
+	 * @return id du lieu
+	 */
 	public int getId() {
 		return id;
 	}
+	
+	/**
+	 * Méthode setter de l'id
+	 * @param id du lieu
+	 */
 	public void setId(int id) {
 		this.id = id;
 	}
+	
+	/**
+	 * Méthode getter du nom
+	 * @return nom du lieu
+	 */
 	public String getNom() {
 		return nom;
 	}
+	
+	/**
+	 * Méthode setter du nom
+	 * @param nom du lieu
+	 */
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
+	
+	/**
+	 * Méthode getter de l'adresse
+	 * @return adresse du lieu
+	 */
 	public String getAdresse() {
 		return adresse;
 	}
+	
+	/**
+	 * Méthode setter de l'adresse
+	 * @param adresse du lieu
+	 */
 	public void setAdresse(String adresse) {
 		this.adresse = adresse;
 	}
+	
+	/**
+	 * Méthode getter des activités
+	 * @return activites du lieu
+	 */
 	public List<Activite> getActivites() {
 		return activites;
 	}
+	
+	/**
+	 * Méthode setter des activités
+	 * @param activites du lieu
+	 */
 	public void setActivites(List<Activite> activites) {
 		this.activites = activites;
 	}
